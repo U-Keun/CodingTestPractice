e5c2157ee502e4eee64c00eb9f2a9aa325018b8b
U-keun Song <sukeun319@gmail.com>
2023-04-03 23:50:49 +0900

    [Silver IV] Title: 스택, Time: 208 ms, Memory: 20816 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,37 +1,58 @@
-import java.util.Scanner;
-import java.util.Stack;
-import static java.lang.Integer.parseInt;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class MyStack {
+    ArrayList<Integer> MyStack = new ArrayList<>();
+    void push(int x) {
+        this.MyStack.add(x);
+    }
+    int pop() {
+        if (!this.MyStack.isEmpty()) {
+            int p = this.MyStack.get(this.MyStack.size() - 1);
+            this.MyStack.remove(this.MyStack.size() - 1);
+            return p;
+        } else return -1;
+    }
+    int size() {
+        return this.MyStack.size();
+    }
+    int empty() {
+        if (this.MyStack.isEmpty()) return 1;
+        else return 0;
+    }
+    int top() {
+        if(this.MyStack.isEmpty()) return -1;
+        else return this.MyStack.get(this.MyStack.size()-1);
+    }
+}
 
 public class Main {
-    public static void main(String[] args) {
-        int a;
-        String b;
-        Scanner scanner = new Scanner(System.in);
-        StringBuffer print = new StringBuffer();
-        a = parseInt(scanner.nextLine());
-        Stack<String> list = new Stack<>();
-        for (int i = 0; i < a; i++){
-            int answer;
-            b = scanner.nextLine();
-            String[] order = b.split(" ");
-            if (order[0].equals("push")) {
-                list.push(order[1]);
-            } else if (order[0].equals("pop")) {
-                if (list.isEmpty()) print.append(-1).append('\n');
-                else {
-                    print.append(list.peek()).append('\n');
-                    list.pop();
-                }
-            } else if (order[0].equals("size")) {
-                print.append(list.size()).append('\n');
-            } else if (order[0].equals("empty")) {
-                if (list.isEmpty()) print.append(1).append('\n');
-                else print.append(0).append('\n');
-            } else {
-                if (list.isEmpty()) print.append(-1).append('\n');
-                else print.append(list.peek()).append('\n');
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder print = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        MyStack stack = new MyStack();
+        for (int i =0 ; i < N; i++) {
+            String[] order = br.readLine().split(" ");
+            switch (order[0]){
+                case "push":
+                    stack.push(Integer.parseInt(order[1]));
+                    break;
+                case "pop":
+                    print.append(stack.pop()).append('\n');
+                    break;
+                case "size":
+                    print.append(stack.size()).append('\n');
+                    break;
+                case "empty":
+                    print.append(stack.empty()).append('\n');
+                    break;
+                case "top":
+                    print.append(stack.top()).append('\n');
+                    break;
             }
-
         }
         System.out.println(print);
     }
