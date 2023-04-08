b8ec3064ebb9662e9d271477707a56ad1ca542d3
U-keun Song <sukeun319@gmail.com>
2023-04-08 12:42:11 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -3,7 +3,7 @@ class Solution {
         long answer = -1;
         long total = 0; // count 번 탔을 때 총 얼마를 내야 하는지
         for (int i=0; i<count; i++) { // price = 3, count = 4
-            total += (long)price * (i+1);
+            total += (long)(price * (i+1));
         }
         answer = (long)total - money;
         if (answer< 0){
