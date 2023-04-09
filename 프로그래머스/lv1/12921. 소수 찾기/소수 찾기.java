28cb042c3de6e9c83c271d4b7924f39e460259c1
U-keun Song <sukeun319@gmail.com>
2023-04-09 13:06:55 +0900

    [level 1] Title: 소수 찾기, Time: 155.09 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean isPrime;
+        for (int i = 2; i <= n; i++) {
+            isPrime = true;
+            for (int j = 2; j <= (int)Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) answer++;
+        }
+        return answer;
+    }
+}
