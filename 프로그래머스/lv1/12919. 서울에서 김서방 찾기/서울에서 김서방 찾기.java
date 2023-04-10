4e990a342648aafc0ad92f7afac13b09aee879f1
U-keun Song <sukeun319@gmail.com>
2023-04-10 09:48:30 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.63 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String[] seoul) {
+        String answer;
+        int idx = Arrays.asList(seoul).indexOf("Kim");
+        answer = "김서방은 " + idx + "에 있다";
+        return answer;
+    }
+}
