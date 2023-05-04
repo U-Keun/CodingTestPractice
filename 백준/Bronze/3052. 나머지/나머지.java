2aa2da8c19a4b74d3c4f199af4ad27b2aca97ee7
U-keun Song <sukeun319@gmail.com>
2023-05-04 09:41:48 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> numbers = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        System.out.println(numbers.size());
+    }
+}
