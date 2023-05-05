510256c1f510b2922bbf384545f8d85278eaa8fd
U-keun Song <sukeun319@gmail.com>
2023-05-06 03:29:13 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int answer = 0;
+        for (int i = 0; i < L; i++) {
+            answer += (input.charAt(i) - 'a' + 1) * power(31, i);
+        }
+        System.out.println(answer);
+   }
+    static int power(int n, int k) {
+        int prod = 1;
+        for (int i = 0; i < k; i++) {
+            prod = prod * n % 1234567891;
+        }
+        if (prod < 0) return prod + 1234567891;
+        else return prod;
+    }
+}
