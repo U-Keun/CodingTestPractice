b6bb1c177ebd4fa2dda810dfd60e8278f63fd4ee
U-keun Song <sukeun319@gmail.com>
2023-05-06 02:42:30 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st2.nextToken());
+        }
+        int answer = 0;
+        int sum;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    sum = numbers[i] + numbers[j] + numbers[k];
+                    if (sum > answer && sum <= M) answer = sum;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
