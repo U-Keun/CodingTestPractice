47fc7da45bf15474fa070a68694617e5a62f6d09
U-keun Song <sukeun319@gmail.com>
2023-05-24 01:25:44 +0900

    [Silver III] Title: N과 M (8), Time: 216 ms, Memory: 21080 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder print = new StringBuilder();
+    static int n, m;
+    static int[] arr;
+    static boolean[] visited;
+    static int[] numbers;
+    static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i:arr) {
+                print.append(i + " ");
+            }
+            print.append('\n');
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            arr[depth] = numbers[i];
+            dfs(depth + 1, i);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        arr = new int[m];
+        visited = new boolean[n];
+        numbers = new int[n];
+        String[] inputNumbers = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(inputNumbers[i]);
+        }
+        Arrays.sort(numbers);
+        dfs(0, 0);
+
+        System.out.println(print);
+    }
+}
