2a275519ecf626226e4c93e94e8bf67d887c354b
U-keun Song <sukeun319@gmail.com>
2023-05-24 23:45:26 +0900

    [Silver IV] Title: 비트 우정지수, Time: 784 ms, Memory: 142364 KB -BaekjoonHub

--- 백준/Silver/12782. 비트 우정지수/비트 우정지수.java
+++ 백준/Silver/12782. 비트 우정지수/비트 우정지수.java
@@ -7,18 +7,16 @@ import java.util.StringTokenizer;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int T;
+    static int T, count;
     static String[] N, M;
+    static Stack<String> stack = new Stack<>();
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
-        Stack<String> stack = new Stack<>();
-        int count;
         for (int i = 0; i < T; i++) {
             count = 0;
             st = new StringTokenizer(br.readLine());
             N = st.nextToken().split("");
             M = st.nextToken().split("");
-
             for (int j = 0; j < N.length; j++) {
                 if (N[j].equals(M[j])) continue;
                 if (stack.isEmpty()) stack.add(N[j]);
