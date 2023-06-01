f996dccef05f28ef0d43bc856f0453c776cb8f14
U-keun Song <sukeun319@gmail.com>
2023-06-01 22:13:31 +0900

    [Silver IV] Title: 오셀로 재배치, Time: 420 ms, Memory: 55524 KB -BaekjoonHub

--- 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.java
+++ 백준/Silver/13413. 오셀로 재배치/오셀로 재배치.java
@@ -6,19 +6,20 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder print = new StringBuilder();
     static int T, N, diffB, diffW;
-    static String[] input, goal;
+    static String input, goal;
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             diffB = 0;
             diffW = 0;
             N = Integer.parseInt(br.readLine());
-            input = br.readLine().split("");
-            goal = br.readLine().split("");
+            input = br.readLine();
+            goal = br.readLine();
             for (int j = 0; j < N; j++) {
-                if (!input[j].equals(goal[j]))
-                    if (input[j].equals("B")) diffB++;
+                if (input.charAt(j) != goal.charAt(j)) {
+                    if (input.charAt(j) == 'B') diffB++;
                     else diffW++;
+                }
             }
             print.append(Math.max(diffB, diffW)).append('\n');
         }
