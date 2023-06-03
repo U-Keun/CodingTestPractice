78394b916f5400c323044734e63c74266684a65a
U-keun Song <sukeun319@gmail.com>
2023-06-03 13:52:07 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static long[] fibonacci = new long[91];
+
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+        }
+        System.out.println(fibonacci[n]);
+    }
+}
