d4ae6fbb7d76dd05896e5a5a5b263a44d670eadf
U-keun Song <sukeun319@gmail.com>
2023-06-05 22:26:01 +0900

    [Silver II] Title: 마인크래프트, Time: 544 ms, Memory: 34964 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class Main { // BOJ 18111
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N, M, B, h, k, plus, minus;
+    static int N, M, B, h, plus, minus, timeTaken;
     static int[][] ground;
+    static int[] answer = new int[2];
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
@@ -16,32 +17,20 @@ public class Main {
         ground = new int[N][M];
         h = 0;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < M; j++) {
-                k = Integer.parseInt(st.nextToken());
-                ground[i][j] = k;
-                h += k;
+                ground[i][j] =Integer.parseInt(st.nextToken());
+                h += ground[i][j];
             }
         }
         h = Math.round((float) h / (N * M));
-        int timeTaken;
-        int[] answer = new int[2];
         do {
             plus = 0;
             minus = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    k = ground[i][j] - h;
-                    if (k > 0) plus += k;
-                    else minus -= k;
-                }
-            }
+            calc(h);
             if (B + plus < minus) break;
             timeTaken = 2 * plus + minus;
-            if (answer[0] == 0) {
-                answer[0] = timeTaken;
-                answer[1] = h;
-            } else if (answer[0] >= timeTaken) {
+            if (answer[0] == 0 || answer[0] >= timeTaken) {
                 answer[0] = timeTaken;
                 answer[1] = h;
             }
@@ -50,16 +39,20 @@ public class Main {
         if (answer[0] == 0) {
             plus = 0;
             minus = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    k = ground[i][j] - h + 1;
-                    if (k > 0) plus += k;
-                    else minus -= k;
-                }
-            }
+            calc(h - 1);
             answer[0] = 2 * plus + minus;
             answer[1] = h - 1;
         }
         System.out.println(answer[0] + " " + answer[1]);
+    }
+    static void calc(int h) {
+        int k;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                k = ground[i][j] - h;
+                if (k > 0) plus += k;
+                else minus -= k;
+            }
+        }
     }
 }
