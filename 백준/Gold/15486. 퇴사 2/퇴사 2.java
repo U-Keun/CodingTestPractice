18822effcb92b640187041fde0ab82c134a2900f
U-keun Song <sukeun319@gmail.com>
2023-07-07 10:08:40 +0900

    [Gold V] Title: 퇴사 2, Time: 720 ms, Memory: 304748 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -6,21 +6,19 @@ import java.util.StringTokenizer;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N;
-    static int[][] table;
+    static int N, T, P;
     static int[] memo;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        table = new int[N][2];
         memo = new int[N + 1];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            table[i][0] = Integer.parseInt(st.nextToken());
-            table[i][1] = Integer.parseInt(st.nextToken());
+            T = Integer.parseInt(st.nextToken());
+            P = Integer.parseInt(st.nextToken());
             memo[i+1] = Math.max(memo[i+1], memo[i]);
-            if(i + table[i][0] <= N) {
-                memo[i + table[i][0]] = Math.max(memo[i + table[i][0]], memo[i] + table[i][1]);
+            if(i + T <= N) {
+                memo[i + T] = Math.max(memo[i + T], memo[i] + P);
             }
         }
         System.out.println(memo[N]);
