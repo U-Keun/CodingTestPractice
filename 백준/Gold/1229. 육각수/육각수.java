3b6eaf38e54939d362b0e825af56e459feb78804
U-keun Song <sukeun319@gmail.com>
2023-07-16 23:05:30 +0900

    [Gold IV] Title: 육각수, Time: 992 ms, Memory: 18328 KB -BaekjoonHub

--- 백준/Gold/1229. 육각수/육각수.java
+++ 백준/Gold/1229. 육각수/육각수.java
@@ -12,19 +12,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         memo = new int[N + 1];
+        for (int i = 1; i < 6; i++) {
+            if (i < N) memo[i] = i;
+        }
         for (int i = 1; i * (2 * i - 1) < N + 1; i++) {
             memo[i * (2 * i - 1)] = 1;
             hexNumbers.add(i * (2 * i - 1));
         }
-        for (int i = 1; i < N + 1; i++) {
-            fillMemo(i);
-        }
+        fillMemo(N);
         System.out.println(memo[N]);
     }
     static void fillMemo(int k) {
-        memo[k] = Integer.MAX_VALUE;
-        for (int i = 0; i < hexNumbers.size() && hexNumbers.get(i) <= k; i++) {
-            memo[k] = Math.min(memo[k], 1 + memo[k - hexNumbers.get(i)]);
+        for (Integer i:hexNumbers) {
+            for (int j = i + 1; j < k + 1; j++) {
+                if (memo[j] == 0) memo[j] = 1 + memo[j - i];
+                else memo[j] = Math.min(memo[j], 1 + memo[j - i]);
+            }
         }
     }
 }
