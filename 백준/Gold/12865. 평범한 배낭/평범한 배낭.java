64f535985e6ff4054acbf716aaf6ec07de739793
U-keun Song <sukeun319@gmail.com>
2023-07-17 13:31:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 15436 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,27 +6,22 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N, K;
-    static int[][] table, memo;
+    static int N, K, w, v;
+    static int[] memo;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        table = new int[N][2];
-        memo = new int[N + 1][K + 1];
+        memo = new int[K + 1];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            table[i][0] = Integer.parseInt(st.nextToken());
-            table[i][1] = Integer.parseInt(st.nextToken());
-        }
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j <= K; j++) {
-                memo[i][j] = memo[i - 1][j];
-                if (table[i - 1][0] > j) continue;
-                memo[i][j] = Math.max(table[i - 1][1] + memo[i - 1][j - table[i - 1][0]], memo[i][j]);
+            w = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            for (int j = K; j >= w; j--) {
+                memo[j] = Math.max(v + memo[j - w], memo[j]);
             }
         }
-        System.out.println(memo[N][K]);
+        System.out.println(memo[K]);
     }
 }
