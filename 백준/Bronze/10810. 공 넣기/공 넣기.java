639d31ac6908b1bff9ba44265c7686005a30e79b
U-keun Song <sukeun319@gmail.com>
2023-09-10 19:35:19 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int N = readInt(), M = readInt();
+        int[] boxes = new int[N + 1];
+        for (int i = 1; i <= M; i++) {
+            int a = readInt(), b = readInt(), c = readInt();
+            for (int j = a; j <= b; j++) {
+                boxes[j] = c;
+            }
+        }
+        StringBuilder print = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            print.append(boxes[i]).append(" ");
+        }
+        System.out.println(print);
+    }
+    public static int readInt() throws IOException {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') {
+            c = System.in.read();
+        }
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        return val;
+    }
+}
