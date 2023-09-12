7eb5a8e018af2c54b43bd6c06290983ffb201b1a
U-keun Song <sukeun319@gmail.com>
2023-09-12 17:11:41 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = readInt();
+        System.out.println((long) n * 4);
+    }
+    public static int readInt() throws IOException {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') c = System.in.read();
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        return val;
+    }
+}
