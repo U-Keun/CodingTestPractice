4e407c77c13228c631494280127012a4d7269086
U-keun Song <sukeun319@gmail.com>
2023-09-13 02:27:31 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = readInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+    public static int readInt() throws IOException {
+        int val = 0;
+        int c = System.in.read();
+        while (c <= ' ') c = System.in.read();
+        do {
+            val = 10 * val + c - 48;
+        } while ((c = System.in.read()) >= 48 && c <= 57);
+        return val;
+    }
+}
