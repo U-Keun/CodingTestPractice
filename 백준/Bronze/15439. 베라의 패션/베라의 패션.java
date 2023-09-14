c20d109ec052efc78e48519673c91e9c1942b462
U-keun Song <sukeun319@gmail.com>
2023-09-15 02:18:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(N * (N - 1)));
+        bw.flush();
+        bw.close();
+    }
+}
