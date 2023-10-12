cc7c19d6a95e0d53c4f5961384589a7716406f96
U-keun Song <sukeun319@gmail.com>
2023-10-12 20:19:25 +0900

    [Silver I] Title: 쿼드트리, Time: 168 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+    private static boolean[][] video;
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int N = Integer.parseInt(br.readLine());
+            video = new boolean[N][N];
+            for (int i = 0; i < N; i++) {
+                char[] input = br.readLine().toCharArray();
+                for (int j = 0; j < N; j++) {
+                    if (input[j] == '1') video[i][j] = true;
+                }
+            }
+            bw.write(recurrence(N, 0, 0));
+            bw.flush();
+        }
+    }
+    private static String recurrence(int N, int i, int j) {
+        if (N == 1) {
+            if (video[i][j]) return "1";
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < 4; k++) {
+            sb.append(recurrence(N / 2, i + (N / 2) * (k / 2), j + (N / 2) * (k % 2)));
+        }
+        boolean isOne = true, isZero = true;
+        for (int k = 0; k < sb.length(); k++) {
+            if (sb.charAt(k) == '1') isZero = false;
+            else isOne = false;
+        }
+        if (isOne) return "1";
+        else if (isZero) return "0";
+        return "(" + sb + ")";
+    }
+}
