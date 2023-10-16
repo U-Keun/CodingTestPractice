8e3e8509dfa2c9df5d66e1efc517bf673477a2a3
U-keun Song <sukeun319@gmail.com>
2023-10-16 19:04:16 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 940 ms, Memory: 67752 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringTokenizer st;
+            StringBuilder print = new StringBuilder();
+            while (true) {
+                st = new StringTokenizer(br.readLine());
+                int n = Integer.parseInt(st.nextToken());
+                if (n == 0) break;
+                int[] histogram = new int[n];
+                for (int i = 0; i < n; i++) {
+                    histogram[i] = Integer.parseInt(st.nextToken());
+                }
+                Stack<Integer> stack = new Stack<>();
+                int[] left = new int[n];
+                for (int i = 0; i < n; i++) {
+                    int idx = i;
+                    while (!stack.isEmpty() && histogram[stack.peek()] >= histogram[i]) {
+                        stack.pop();
+                        idx = left[idx];
+                    }
+                    if (stack.isEmpty()) left[i] = 0;
+                    else left[i] = stack.peek() + 1;
+                    stack.push(i);
+                }
+                stack.clear();
+                int[] right = new int[n];
+                for (int i = n - 1; i >= 0; i--) {
+                    int idx = i;
+                    while (!stack.isEmpty() && histogram[stack.peek()] >= histogram[i]) {
+                        stack.pop();
+                        idx = right[idx];
+                    }
+                    if (stack.isEmpty()) right[i] = n;
+                    else right[i] = stack.peek();
+                    stack.push(i);
+                }
+                long max = -1;
+                for (int i = 0; i < n; i++) {
+                    max = Math.max(max, (long) histogram[i] * (right[i] - left[i]));
+                }
+                print.append(max).append('\n');
+            }
+            bw.write(print.toString());
+            bw.flush();
+        }
+    }
+}
