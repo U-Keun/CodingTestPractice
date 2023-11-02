1d3a0c54ef62808710e5e2c03ddb20de7a35b5e6
U-keun Song <sukeun319@gmail.com>
2023-11-02 17:15:55 +0900

    [Silver III] Title: 카드 놓기, Time: 1016 ms, Memory: 202840 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.java
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.java
@@ -3,42 +3,42 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             int N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            Queue<Integer> firstQueue = new LinkedList<>();
-            Queue<Integer> secondQueue = new LinkedList<>();
-            Stack<Integer> thirdStack = new Stack<>();
-            for (int i = N; i > 0; i--) {
-                switch (Integer.parseInt(st.nextToken())) {
+            List<Integer> command = Arrays.stream(br.readLine().split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            Deque<Integer> deque = new ArrayDeque<>();
+            for (int i = N - 1; i >= 0; i--) {
+                switch (command.get(i)) {
                     case 1:
-                        firstQueue.add(i);
-                        while (!secondQueue.isEmpty()) {
-                            firstQueue.add(secondQueue.poll());
-                        }
+                        deque.addFirst(N - i);
                         break;
                     case 2:
-                        secondQueue.add(i);
+                        if (!deque.isEmpty()) {
+                            int tmp = deque.pollFirst();
+                            deque.addFirst(N - i);
+                            deque.addFirst(tmp);
+                        } else deque.addFirst(N - i);
                         break;
                     case 3:
-                        thirdStack.push(i);
+                        deque.addLast(N - i);
                         break;
                 }
             }
             StringBuilder print = new StringBuilder();
-            while (!firstQueue.isEmpty()) {
-                print.append(firstQueue.poll()).append(" ");
-            }
-            while (!thirdStack.isEmpty()) {
-                print.append(thirdStack.pop()).append(" ");
+            print.append(deque.pollFirst());
+            while (!deque.isEmpty()) {
+                print.append(" ").append(deque.pollFirst());
             }
             bw.write(print.toString());
             bw.flush();
