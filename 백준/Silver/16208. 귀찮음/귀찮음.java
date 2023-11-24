c9891024b951faf10dd6f22fc62d385ad05aebfa
U-keun Song <sukeun319@gmail.com>
2023-11-24 16:22:22 +0900

    [Silver V] Title: 귀찮음, Time: 1608 ms, Memory: 321592 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.java
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -4,26 +4,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static Integer answer = 0;
+    private static Long answer = 0L;
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             int N = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            List<Integer> numbers = new ArrayList<>();
+            List<Long> numbers = new ArrayList<>();
             for (int i = 0; i < N; i++) {
-                numbers.add(Integer.parseInt(st.nextToken()));
+                numbers.add(Long.parseLong(st.nextToken()));
             }
+            Collections.sort(numbers);
             setCost(numbers);
             bw.write(String.valueOf(answer));
             bw.flush();
         }
     }
-    private static void setCost(List<Integer> numbers) {
+    private static void setCost(List<Long> numbers) {
         int l = numbers.size();
         if (l == 1) {
             return;
@@ -32,15 +34,14 @@ public class Main {
             answer += numbers.get(0) * numbers.get(1);
             return;
         }
-        List<Integer> head = numbers.subList(0, (l + 1) / 2);
-        List<Integer> tail = numbers.subList((l + 1) / 2, l);
+        List<Long> head = numbers.subList(0, (l + 1) / 2);
+        List<Long> tail = numbers.subList((l + 1) / 2, l);
         answer += sum(head) * sum(tail);
         setCost(head);
         setCost(tail);
     }
-    private static Integer sum(List<Integer> numbers) {
+    private static Long sum(List<Long> numbers) {
         return numbers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+                .reduce(0L, Long::sum);
     }
 }
