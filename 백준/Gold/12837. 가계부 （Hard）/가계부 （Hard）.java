8b27aee6bb02b7aeb2edaa5755cc3bc47af9b654
U-keun Song <sukeun319@gmail.com>
2024-02-11 16:52:10 +0900

    [Gold I] Title: 가계부 (Hard), Time: 584 ms, Memory: 53088 KB -BaekjoonHub

--- 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.java
+++ 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.java
@@ -6,43 +6,41 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int N;
     private static long[] tree;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()), Q = Integer.parseInt(st.nextToken());
-        int height = (int) Math.ceil(Math.log(N) / Math.log(2));
-        tree = new long[2 << height];
+        N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        tree = new long[N + 1];
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
             int q = Integer.parseInt(st.nextToken());
             if (q == 1) {
-                update(1, 0, N - 1,
-                        Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()));
+                update(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             } else {
-                bw.write(getPrefixSum(1, 0, N - 1,
-                        Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1) + "\n");
+                int l = Integer.parseInt(st.nextToken()), r = Integer.parseInt(st.nextToken());
+                 bw.write(getPrefixSum(r) - getPrefixSum(l - 1) + "\n");
             }
         }
         br.close();
         bw.flush();
         bw.close();
     }
-    private static void update(int current, int start, int end, int index, int money) {
-        if (index < start || index > end) return;
-        tree[current] += money;
-        if (start != end) {
-            int mid = (start + end) >> 1;
-            update(current * 2, start, mid, index, money);
-            update(current * 2 + 1, mid + 1, end, index, money);
+    private static void update(int index, int money) {
+        while (index < N + 1) {
+            tree[index] += money;
+            index += (index & -index);
         }
     }
-    private static long getPrefixSum(int current, int start, int end, int left, int right) {
-        if (left > end || right < start) return 0L;
-        if (left <= start && right >= end) return tree[current];
-        int mid = (start + end) >> 1;
-        return getPrefixSum(current * 2, start, mid, left, right) +
-                getPrefixSum(current * 2 + 1, mid + 1, end, left, right);
+    private static long getPrefixSum(int index) {
+        long answer = 0;
+        while (index > 0) {
+            answer += tree[index];
+            index -= (index & -index);
+        }
+        return answer;
     }
 }
