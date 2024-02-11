989c7cba69dd457ef93d53c4ecbc547c3a66b096
U-keun Song <sukeun319@gmail.com>
2024-02-11 17:08:05 +0900

    [Gold I] Title: 커피숍2, Time: 844 ms, Memory: 73232 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N;
+    private static long[] tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+        tree = new long[N + 1];
+        long[] numbers = new long[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Long.parseLong(st.nextToken());
+            update(i, numbers[i]);
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+            int left = Math.min(x, y), right = Math.max(x, y);
+            bw.write(getPrefixSum(right) - getPrefixSum(left - 1) + "\n");
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            update(a, b - numbers[a]);
+            numbers[a] = b;
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    private static void update(int index, long diff) {
+        while (index < N + 1) {
+            tree[index] += diff;
+            index += (index & -index);
+        }
+    }
+    private static long getPrefixSum(int index) {
+        long answer = 0;
+        while (index > 0) {
+            answer += tree[index];
+            index -= (index & -index);
+        }
+        return answer;
+    }
+}
