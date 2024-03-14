a4017a5ae7ce96b5174e791266954e4f44a6073d
U-keun Song <sukeun319@gmail.com>
2024-03-14 19:12:55 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14112 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]), M = Integer.parseInt(input[1]);
+        System.out.println((N + M) * (N - M));
+    }
+}
