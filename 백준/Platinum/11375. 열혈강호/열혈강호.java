247a7c7222941f5062ef33a2b49478e27266d76b
U-keun Song <sukeun319@gmail.com>
2024-03-21 16:10:57 +0900

    [Platinum IV] Title: 열혈강호, Time: 1504 ms, Memory: 98768 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static List<Integer>[] graph;
+    private static boolean[] visited;
+    private static int[] record;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int edges = Integer.parseInt(st.nextToken());
+            int node;
+            for (int j = 0; j < edges; j++) {
+                node = Integer.parseInt(st.nextToken());
+                graph[i].add(node);
+            }
+        }
+        br.close();
+        record = new int[M + 1];
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[M + 1];
+            if (findMatching(i)) answer++;
+        }
+        System.out.println(answer);
+    }
+    private static boolean findMatching(int cow) {
+        List<Integer> tasks = graph[cow];
+        for (Integer task : tasks) {
+            if (visited[task]) continue;
+            visited[task] = true;
+            if (record[task] == 0 || findMatching(record[task])) {
+                record[task] = cow;
+                return true;
+            }
+        }
+        return false;
+    }
+}
