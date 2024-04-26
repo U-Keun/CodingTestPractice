285531f55eaad8e7b0b9dd5a126ab95b2f11ed81
U-keun Song <sukeun319@gmail.com>
2024-04-26 10:38:16 +0900

    [Gold IV] Title: 줄세우기, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(N - max);
+    }
+}
