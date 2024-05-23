52b681f668e46a4a91fea319c7711f27d70d6944
U-keun Song <sukeun319@gmail.com>
2024-05-23 16:06:59 +0900

    [Silver I] Title: 행렬 곱셈 순서 4, Time: 1004 ms, Memory: 301040 KB -BaekjoonHub

--- 백준/Silver/25683. 행렬 곱셈 순서 4/행렬 곱셈 순서 4.java
+++ 백준/Silver/25683. 행렬 곱셈 순서 4/행렬 곱셈 순서 4.java
@@ -7,18 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] row = new int[2], sizes = new int[N + 1];
+        int[] row;
+        long answer = 0;
         for (int i = 0; i < N; i++) {
             row = readIntegers(br.readLine());
-            sizes[i] = row[0];
+            if (i < N - 1) {
+                answer += (long) row[0] * row[1];
+                continue;
+            }
+            answer *= row[1];
         }
         br.close();
-        sizes[N] = row[1];
-        long answer = 0;
-        for (int i = 0; i < N - 1; i++) {
-            answer += (long) sizes[i] * sizes[i + 1];
-        }
-        answer *= sizes[N];
         System.out.println(answer);
     }
     private static int[] readIntegers(String input) {
