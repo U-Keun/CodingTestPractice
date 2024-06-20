9908e74410151598ba33b1c3cb71b3f147212194
U-keun Song <sukeun319@gmail.com>
2024-06-20 17:10:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 142568 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,44 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int N, M, components;
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N];
-        visited = new boolean[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()),
+            M = Integer.parseInt(st.nextToken());
+        List<List<Integer>> adjList = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            graph[i] = new ArrayList<>();
+            adjList.add(new ArrayList<>());
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int tmp1 = Integer.parseInt(st.nextToken());
-            int tmp2 = Integer.parseInt(st.nextToken());
-            graph[tmp1 - 1].add(tmp2);
-            graph[tmp2 - 1].add(tmp1);
+            int u = Integer.parseInt(st.nextToken()),
+                v = Integer.parseInt(st.nextToken());
+
+            adjList.get(u - 1).add(v - 1);
+            adjList.get(v - 1).add(u - 1);
         }
-        components = 0;
+
+        boolean[] visited = new boolean[N];
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        int answer = 0;
         for (int i = 0; i < N; i++) {
             if (visited[i]) continue;
-            components++;
-            dfs(i);
+            answer++;
+            visited[i] = true;
+            queue.addLast(i);
+            while (!queue.isEmpty()) {
+                int tmp = queue.pollLast();
+                visited[tmp] = true;
+                for (int v : adjList.get(tmp)) {
+                    if (visited[v]) continue;
+                    visited[v] = true;
+                    queue.addLast(v);
+                }
+            }
         }
-        System.out.println(components);
-    }
-    static void dfs(int i) {
-        if (visited[i]) return;
-        visited[i] = true;
-        for (int j: graph[i]) {
-            dfs(j - 1);
-        }
+
+        System.out.println(answer);
     }
 }
