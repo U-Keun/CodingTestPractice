e79fd1bd9a189af996a52f5d225384f2c0813dd7
U-keun Song <sukeun319@gmail.com>
2024-06-20 17:01:40 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 652 ms, Memory: 60648 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,48 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder print = new StringBuilder();
-    static int N, v1, v2, parent;
-    static String input;
-    static String[] splitInput;
-    static Map<Integer, List<Integer>> graph;
-    static Queue<Integer> vertices;
-    static int[] parents;
-
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        graph = new HashMap<>();
-        vertices = new LinkedList<>();
-        parents = new int[N - 1];
-        while ((input = br.readLine()) != null) {
-            splitInput = input.split(" ");
-            v1 = Integer.parseInt(splitInput[0]);
-            v2 = Integer.parseInt(splitInput[1]);
-            putEdge(v1, v2);
-            putEdge(v2, v1);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            adjList.add(new ArrayList<>());
         }
-        vertices.add(1);
-        while (!vertices.isEmpty()) {
-            parent = vertices.poll();
-            for (int i:graph.getOrDefault(parent, new ArrayList<>(){})) {
-                if (i == 1 || parents[i - 2] != 0) continue;
-                parents[i - 2] = parent;
-                vertices.add(i);
+
+        StringTokenizer st;
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()),
+                v = Integer.parseInt(st.nextToken());
+
+            adjList.get(u - 1).add(v - 1);
+            adjList.get(v - 1).add(u - 1);
+        }
+
+        boolean[] visited = new boolean[N];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(0);
+
+        int[] record = new int[N];
+        while (!queue.isEmpty()) {
+            int tmp = queue.pollLast();
+            visited[tmp] = true;
+            for (int v : adjList.get(tmp)) {
+                if (visited[v]) continue;
+                visited[v] = true;
+                record[v] = tmp;
+                queue.addLast(v);
             }
         }
-        for (int i:parents) {
-            print.append(i).append('\n');
+
+        StringBuilder print = new StringBuilder();
+        for (int i = 1; i < N; i++) {
+            print.append(record[i] + 1);
+            if (i != N - 1) print.append("\n");
         }
         System.out.println(print);
-    }
-    static void putEdge(int v1, int v2) {
-        if (!graph.containsKey(v1)) {
-            graph.put(v1, new ArrayList<>());
-        }
-        graph.get(v1).add(v2);
     }
 }
