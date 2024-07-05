53747162d7b3f5dc0cf950de0a3e14ca7c87db47
U-keun Song <sukeun319@gmail.com>
2024-07-05 16:27:46 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+long long mod_power(long long n, long long k, long long mod);
+
+int main() {
+    long long a, b, c;
+    scanf("%lld %lld %lld", &a, &b, &c);
+    
+    printf("%lld\n", mod_power(a, b, c));
+
+    return 0;
+}
+
+long long mod_power(long long n, long long k, long long mod) {
+    long long answer = 1;
+    n %= mod;
+    while (k > 0) {
+        if (k % 2 == 1) {
+            answer = (answer * n) % mod;
+        }
+        n = (n * n) % mod;
+        k /= 2;
+    }
+    return answer % mod;
+}
