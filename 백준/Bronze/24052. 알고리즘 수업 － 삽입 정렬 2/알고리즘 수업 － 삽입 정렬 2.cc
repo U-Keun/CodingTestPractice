b9eb7c44cf65889290cec44138227ab56af49ea5
U-keun Song <sukeun319@gmail.com>
2024-07-11 23:38:01 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 2, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24052. 알고리즘 수업 － 삽입 정렬 2/알고리즘 수업 － 삽입 정렬 2.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    int array[N];
+    for (int i = 0; i < N; i++) {
+        cin >> array[i];
+    }
+    
+    for (int i = 1; i < N; i++) {
+        int loc = i - 1;
+        int new_item = array[i];
+        
+        while (0 <= loc && new_item < array[loc]) {
+            array[loc + 1] = array[loc];
+            if (--K == 0) {
+                for (int i = 0; i < N; i++) {
+                    cout << array[i] << " ";
+                }
+                cout << '\n';
+                return 0;
+            }
+            loc--;
+        }
+        if (loc + 1 != i) {
+            array[loc + 1] = new_item;
+            if (--K == 0) {
+                for (int i = 0; i < N; i++) {
+                    cout << array[i] << " ";
+                }
+                cout << '\n';
+                return 0;
+            }
+        }
+    }
+    
+    if (K > 0) {
+        cout << -1 << '\n';
+    }
+    
+    return 0;
+}
