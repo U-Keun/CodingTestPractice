cc2278c2ca571c0c36ea08bb1795c26f5dd65f71
U-keun Song <sukeun319@gmail.com>
2024-07-14 00:28:59 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    cout << N * M - 1 << '\n';
+}
