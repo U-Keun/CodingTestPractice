d3a36f0036d774526081faae0e5c22d76b204fe7
U-keun Song <sukeun319@gmail.com>
2024-07-20 15:42:25 +0900

    [Silver III] Title: 정사각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1485. 정사각형/정사각형.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+    if (a.second == b.second) {
+        return a.first < b.first;
+    }
+    return a.second < b.second;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        vector<pair<int, int>> points;
+        for (int i = 0; i < 4; ++i) {
+            int x, y;
+            cin >> x >> y;
+            points.push_back({x, y});
+        }
+
+        sort(points.begin(), points.end(), compare);
+
+        int c1x = (points[0].first + points[3].first);
+        int c1y = (points[0].second + points[3].second);
+        int c2x = (points[1].first + points[2].first);
+        int c2y = (points[1].second + points[2].second);
+
+        if (c1x != c2x || c1y != c2y) {
+            cout << "0" << '\n';
+            continue;
+        }
+
+        int dx1 = points[3].first - points[0].first;
+        int dx2 = points[2].first - points[1].first;
+        int dy1 = points[3].second - points[0].second;
+        int dy2 = points[2].second - points[1].second;
+
+        long long tangentCheck = dy1 * dy2 + dx2 * dx1;
+        if (tangentCheck != 0 ||
+            (dx1 * dx1 + dy1 * dy1 != dx2 * dx2 + dy2 * dy2)) {
+            cout << "0" << '\n';
+            continue;
+        }
+        
+        cout << "1" << '\n';
+        points.clear();
+    }
+    return 0;
+}
