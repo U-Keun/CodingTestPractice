8dbd66795a05de6350e01e38478849dd9e9627bf
U-keun Song <sukeun319@gmail.com>
2024-07-25 00:24:53 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int R1, S;
+    cin >> R1 >> S;
+    cout << 2 * S - R1 << '\n';
+
+    return 0;
+}
