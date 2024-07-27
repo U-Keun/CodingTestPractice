f77fb52f938f607e7a206e539b18a064e4bb3e65
U-keun Song <sukeun319@gmail.com>
2024-07-27 15:50:53 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,42 +1,49 @@
 #include <iostream>
 #include <vector>
+#define FAST_IO() \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); \
+    cout.tie(NULL);
+#define ll long long
 
 using namespace std;
 
+bool canMoveDiagonally(const vector<vector<int> >& house, int i, int j) {
+    return house[i - 1][j] != 1 && house[i][j - 1] != 1;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    FAST_IO();
 
     int n;
     cin >> n;
 
-    vector<vector<int>> house(n, vector<int>(n));
+    vector<vector<int> > house(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> house[i][j];
         }
     }
 
-    vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(3, 0))); // 각 지점에서 길이 3짜리 배열은 각각 가로로, 대각선으로, 세로로 놓인 경우들을 의미한다.
-
+    vector<vector<vector<ll> > > dp(3, vector<vector<ll> >(n, vector<ll>(n, 0)));
     for (int i = 1; i < n; i++) {
         if (house[0][i] == 1) break;
-        dp[0][i][0] = 1; // 집의 가장 윗부분(인덱스가 house[0] 부분)은 가로로 움직일 수 밖에 없다..!
+        dp[0][0][i] = 1;
     }
 
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < n; j++) {
             if (house[i][j] != 1) {
-                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1];
-                if (house[i - 1][j] != 1 && house[i][j - 1] != 1) {
-                     dp[i][j][1] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+                dp[0][i][j] = dp[0][i][j - 1] + dp[1][i][j - 1];
+                if (canMoveDiagonally(house, i, j)) {
+                    dp[1][i][j] = dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1];
                 }
-                dp[i][j][2] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                dp[2][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j];
             }
         }
     }
 
-    cout << dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2] << '\n';
+    cout << dp[0][n - 1][n - 1] + dp[1][n - 1][n - 1] + dp[2][n - 1][n - 1] << '\n';
 
     return 0;
 }
