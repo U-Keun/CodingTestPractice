46b4ac32468b6628481e05021a108ad7b4c5e6da
U-keun Song <sukeun319@gmail.com>
2024-08-22 23:51:04 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -12,18 +12,18 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<short>> graph(n, vector<short>(n, 0));
+    vector<vector<bool>> graph(n, vector<bool>(n, 0));
     int u, v;
     for (int i = 0; i < m; i++) {
         cin >> u >> v;
-        graph[u - 1][v - 1] = 1;
+        graph[u - 1][v - 1] = true;
     }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             for (int k = 0; k < n; k++) {
-                if (graph[j][i] * graph[i][k] > 0) {
-                    graph[j][k] = 1;
+                if (graph[j][i] && graph[i][k]) {
+                    graph[j][k] = true;
                 }
             }
         }
@@ -33,8 +33,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         int lighter = 0, heavier = 0;
         for (int j = 0; j < n; j++) {
-            if (graph[i][j] == 1) heavier++;
-            else if (graph[j][i] == 1) lighter++;
+            if (graph[i][j]) heavier++;
+            else if (graph[j][i]) lighter++;
         }
 
         if (heavier > n / 2 || lighter > n / 2) answer++;
