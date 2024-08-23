5fba98f2abf465d377a0597d8f9fdef2affad2d7
U-keun Song <sukeun319@gmail.com>
2024-08-23 12:58:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 3.29 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+bool isValidPth(queue<char> charQueue) {
+    stack<char> stack;
+    while (!charQueue.empty()) {
+        char cur = charQueue.front();
+        charQueue.pop();
+        if (cur == '(' || cur == '{' || cur == '[') {
+                stack.push(cur);
+                continue;
+        }
+        if (stack.empty()) {
+            if (cur == ')' || cur == '}' || cur == ']') return false;
+        } else {
+            if (stack.top() == '(' && cur == ')') stack.pop();
+            else if (stack.top() == '{' && cur == '}') stack.pop();
+            else if (stack.top() == '[' && cur == ']') stack.pop();
+            else return false;
+        }
+    }
+    
+    return stack.empty();
+}
+
+int solution(string s) {
+    queue<char> charQueue;
+    for (char c : s) charQueue.push(c);
+    
+    int l = s.size(), answer = 0;;
+    for (int i = 0; i < l; i++) {
+        if (isValidPth(charQueue)) answer++;
+        charQueue.push(charQueue.front());
+        charQueue.pop();
+    }
+    
+    return answer;
+}
