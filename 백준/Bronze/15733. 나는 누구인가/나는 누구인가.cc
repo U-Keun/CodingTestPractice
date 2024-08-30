393628809f6acf33e1c2c96131ceb42192d7f07d
U-keun Song <sukeun319@gmail.com>
2024-08-31 05:33:28 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+
+using namespace std;
+
+int main() {
+// int algorithm() {
+    FAST_IO
+
+    cout << "I'm Sexy";
+
+    return 0;
+}
