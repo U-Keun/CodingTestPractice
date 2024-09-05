64fba5b88a78ac64ffe6a7f65ac033edc65378c4
U-keun Song <sukeun319@gmail.com>
2024-09-05 16:36:28 +0900

    [level 3] Title: 등대, Time: 46.81 ms, Memory: 27.4 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.cpp
+++ 프로그래머스/3/133500. 등대/등대.cpp
@@ -1,46 +1,37 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <functional>
 
 using namespace std;
 
-#define REP(i,a,b) for (int i = a; i <= b; i++)
-
-int answer = 0;
-vector<vector<int>> graph;
-
-int dfs(int prev, int current) {
-    if (prev >= 0 && graph[current].size() == 1) {
-        return 1;
-    }
-    
-    int decider = 0;
-    for (int nbd : graph[current]) {
-        if (nbd == prev) continue;
-        decider += dfs(current, nbd);
-    }
-    
-    if (decider > 0) {
-        answer++;
-        return 0;
-    }
-    
-    return 1;
-}
-
 int solution(int n, vector<vector<int>> lighthouse) {
-    
-    // graph.clear();
-    
-    REP(i, 0, n - 1) {
-        vector<int> nbds;
-        graph.push_back(nbds);
-    } 
+    vector<vector<int>> graph(n, vector<int>());
     
     for (vector<int> edge : lighthouse) {
         graph[edge[0] - 1].emplace_back(edge[1] - 1);
         graph[edge[1] - 1].emplace_back(edge[0] - 1);
     }
+    
+    int answer = 0;
+    
+    function<int(int, int)> dfs = [&](int prev, int cur) -> int {
+        if (prev >= 0 && graph[cur].size() == 1) {
+            return 1;
+        }
+        
+        int decider = 0;
+        for (int nbd : graph[cur]) {
+        	if (nbd == prev) continue;
+        	decider += dfs(cur, nbd);
+    	}
+    
+    	if (decider > 0) {
+        	answer++;
+        	return 0;
+    	}
+    	return 1;
+    };
+    
     
     dfs(-1, 0);
     
