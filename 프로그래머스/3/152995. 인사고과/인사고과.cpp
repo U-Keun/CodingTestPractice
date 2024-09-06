b2d93c632fa98cb122df056ed738494519c19061
U-keun Song <sukeun319@gmail.com>
2024-09-06 13:34:11 +0900

    [level 3] Title: 인사고과, Time: 770.02 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 #define REP(i,a,b) for (int i = a; i <= b; i++)
+#define REP_D(i,a,b) for (int i = a; i >= b; i--)
 
 int solution(vector<vector<int>> scores) {
     vector<vector<int>> candidates;
@@ -22,11 +23,11 @@ int solution(vector<vector<int>> scores) {
     sort(candidates.begin(), candidates.end());
     int m = candidates.size(), deleteCount = 0;
     REP(i, 0, m - 1) {
-        REP(j, i + 1, m - 1) {
-	    if (candidates[i][0] == candidates[j][0]) continue;
+        REP_D(j, m - 1, i + 1) {
+			if (candidates[i][0] == candidates[j][0]) continue;
             if (candidates[i][1] < candidates[j][1]) {
-		deleteCount++;
-		break;            
+				deleteCount++;
+				break;            
             }
         }
     }
