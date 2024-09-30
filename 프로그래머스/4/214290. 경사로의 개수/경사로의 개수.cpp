8806b26ecc581d47eb751f48e8c3420fe49d30e1
U-keun Song <sukeun319@gmail.com>
2024-10-01 02:14:22 +0900

    Update 경사로의 개수.cpp

--- 프로그래머스/4/214290. 경사로의 개수/경사로의 개수.cpp
+++ 프로그래머스/4/214290. 경사로의 개수/경사로의 개수.cpp
@@ -18,7 +18,7 @@ bool isValidPos(int r, int c) {
 }
 
 vector<vector<ll>> matrix_product(vector<vector<ll>> A, vector<vector<ll>> B) {
-	int n = A.size();
+    int n = A.size();
     vector<vector<ll>> answer(n, vector<ll>(n, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -33,7 +33,7 @@ vector<vector<ll>> matrix_product(vector<vector<ll>> A, vector<vector<ll>> B) {
 }
 
 vector<vector<ll>> matrix_power(vector<vector<ll>> matrix, long long k) {
-	int n = matrix.size();
+    int n = matrix.size();
     vector<vector<ll>> res(n, vector<ll>(n));
     for (int i = 0; i < n; i++) {
         res[i][i] = 1;
@@ -52,25 +52,27 @@ int solution(vector<vector<int>> grid, vector<int> d, int k) {
     row = grid.size();
     col = grid[0].size();
     int l = d.size();
-    vector<vector<vector<ll>>> dp(l + 1, vector<vector<ll>>(row * col, vector<ll>(row * col)));
-    REP(i, 0, row * col - 1) dp[0][i][i] = 1;
+    vector<vector<ll>> dp(row * col, vector<ll>(row * col, 0));
+    REP(i, 0, row * col - 1) dp[i][i] = 1;
     
     REP(i, 1, l) {
+        vector<vector<ll>> next_dp(row * col, vector<ll>(row * col, 0));
         REP(j, 0, row * col - 1) {
             int curRow = j / col, curCol = j % col;
             REP(k, 0, 3) {
                 int r = curRow + dy[k], c = curCol + dx[k];
                 if (isValidPos(r, c) && grid[r][c] == grid[curRow][curCol] + d[i - 1]) {
                     REP(m, 0, row * col - 1) {
-                        dp[i][m][r * col + c] += dp[i - 1][m][j] % MOD;
-                        dp[i][m][r * col + c] %= MOD;
+                        next_dp[m][r * col + c] += dp[m][j];
+                        next_dp[m][r * col + c] %= MOD;
                     }
                 }
             }
         }
+        dp = next_dp;
     }
     
-    vector<vector<ll>> result = matrix_power(dp[l], k);
+    vector<vector<ll>> result = matrix_power(dp, k);
     ll answer = 0;
     REP(i, 0, row * col - 1) {
         REP(j, 0, row * col - 1) {
