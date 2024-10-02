796190f69dab7511b58a6842367d4807441bb3f2
U-keun Song <sukeun319@gmail.com>
2024-10-02 13:45:22 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -13,21 +13,18 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<int> pack(m), one(m);
+    int one = 1001, pack = 1001, tmpOne, tmpPack;
     REP(i, 0, m - 1) {
-        cin >> pack[i] >> one[i];
+        cin >> tmpPack >> tmpOne;
+        pack = min(pack, tmpPack);
+        one = min(one, tmpOne);
     }
 
-    sort(pack.begin(), pack.end());
-    sort(one.begin(), one.end());
-
-    int packs = n / 6;
-
-    int answer = packs * ((6 * one[0] > pack[0]) ? pack[0] : 6 * one[0]);
+    int answer = (n / 6) * ((6 * one > pack) ? pack : 6 * one);
 
     int res = n % 6;
     if (res != 0) {
-        answer += (res * one[0] > pack[0]) ? pack[0] : res * one[0];
+        answer += (res * one > pack) ? pack : res * one;
     }
 
     cout << answer;
