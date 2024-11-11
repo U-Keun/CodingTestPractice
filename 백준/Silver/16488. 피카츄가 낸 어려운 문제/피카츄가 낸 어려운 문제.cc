69ad13c4b74c5329af67e722efcf8421b651c237
U-Keun Song <sukeun319@gmail.com>
2024-11-11 19:03:15 +0900

    [Silver IV] Title: 피카츄가 낸 어려운 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16488. 피카츄가 낸 어려운 문제/피카츄가 낸 어려운 문제.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+
+using namespace std;
+
+int main() {
+    FAST_IO
+
+    long long n, k;
+    cin >> n >> k;
+    cout << n * n * k;
+
+    return 0;
+}
