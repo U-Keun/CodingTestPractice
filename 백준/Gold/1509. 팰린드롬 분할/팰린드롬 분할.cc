e15ef35fa32ff3bf4d08d1226e267cc79b30ebbf
U-Keun Song <sukeun319@gmail.com>
2024-12-01 17:25:08 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 24 ms, Memory: 3040 KB -BaekjoonHub

---
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+
+using namespace std;
+
+vector<vector<bool>> record;
+
+int main() {
+    FAST_IO
+
+    string input;
+    cin >> input;
+
+    int l = input.size();
+    record.resize(l, vector<bool>(l, false));
+
+    for (int i = 0; i < l; i++) record[i][i] = true;
+    for (int i = 1; i < l; i++) record[i - 1][i] = (input[i - 1] == input[i]);
+
+    for (int i = l - 3; i >= 0; i--) {
+        for (int j = i + 2; j < l; j++) {
+            if (input[i] == input[j]) record[i][j] = record[i + 1][j - 1];
+        }
+    }
+
+    vector<int> dp(l + 1, l);
+    dp[0] = 0;
+    for (int j = 0; j < l; j++) {
+        for (int i = 0; i <= j; i++) {
+            if (!record[i][j]) continue;
+            dp[j + 1] = min(dp[j + 1], dp[i] + 1);
+        }
+    }
+
+    cout << dp[l];
+
+
+    return EXIT_SUCCESS;
+}
