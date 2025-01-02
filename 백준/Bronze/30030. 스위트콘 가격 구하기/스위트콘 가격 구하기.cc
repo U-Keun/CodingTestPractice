7da198b452f1abf0a4e46e5cd5cfc7acd5fe1a9b
U-Keun Song <sukeun319@gmail.com>
2025-01-03 02:31:42 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+
+using namespace std;
+
+int main() {
+    FAST_IO
+
+    int b; cin >> b;
+    cout << (b / 11 * 10);
+
+    return EXIT_SUCCESS;
+}
