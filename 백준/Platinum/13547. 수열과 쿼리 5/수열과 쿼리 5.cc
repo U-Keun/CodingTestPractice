6fcdf11b14db0fb56fcc5f35bbfb09e5d723f5ba
U-Keun Song <sukeun319@gmail.com>
2025-01-30 10:17:44 +0900

    [Platinum II] Title: 수열과 쿼리 5, Time: 168 ms, Memory: 8772 KB -BaekjoonHub

---
+++ 백준/Platinum/13547. 수열과 쿼리 5/수열과 쿼리 5.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+#define FAST_IO cin.tie(NULL)->ios::sync_with_stdio(false);
+
+using namespace std;
+
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define NUM_LIMIT 1000001
+
+vector<int> arr, frequency;
+int n, sqrt_n, distinct;
+
+struct query {
+    int idx, left, right;
+    bool operator < (const query &x) const {
+        if (left / sqrt_n != x.left / sqrt_n) return left / sqrt_n < x.left / sqrt_n;
+        return right < x.right;
+    }
+};
+
+void add_frequency(int x) {
+    if (frequency[x] == 0) distinct++;
+    frequency[x]++;
+}
+
+void remove_frequency(int x) {
+    if (frequency[x] == 1) distinct--;
+    frequency[x]--;
+}
+
+int main() {
+    FAST_IO
+
+    cin >> n;
+    sqrt_n = sqrt(n);
+    arr.resize(n);
+    REP(i, 0, n - 1) cin >> arr[i];
+
+    frequency.resize(NUM_LIMIT, 0);
+    distinct = 0;
+
+    vector<query> queries;
+    int m; cin >> m;
+    REP(i, 0, m - 1) {
+        int l, r; cin >> l >> r;
+        l--; r--;
+        queries.push_back({ i, l, r });
+    }
+
+    sort(queries.begin(), queries.end());
+
+    int cur_left = 0, cur_right = -1;
+    vector<int> answer(m);
+    for (const auto &q : queries) {
+        int l = q.left, r = q.right;
+        while (cur_right < r) add_frequency(arr[++cur_right]);
+        while (cur_right > r) remove_frequency(arr[cur_right--]);
+        while (cur_left < l) remove_frequency(arr[cur_left++]);
+        while (cur_left > l) add_frequency(arr[--cur_left]);
+
+        answer[q.idx] = distinct;
+    }
+
+    REP(i, 0, m - 1) cout << answer[i] << '\n';
+
+    return EXIT_SUCCESS;
+}
