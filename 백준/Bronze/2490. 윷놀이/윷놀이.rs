42fa6796854f18c3ccdb1ff45c66c97e9964f88e
U-Keun Song <sukeun319@gmail.com>
2025-04-14 17:26:27 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.rs
+++ 백준/Bronze/2490. 윷놀이/윷놀이.rs
@@ -1,21 +1,15 @@
 use std::io;
 
 fn main() {
-    for _i in 0..3 {
+    let results = ["D", "C", "B", "A", "E"];
+
+    for _ in 0..3 {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let input = input.trim();
+        let count = input.trim().chars().filter(|&c| c == '1').count();
 
-        let count = input.chars().filter(|&c| c == '1').count();
-
-        match count {
-            0 => println!("D"),
-            1 => println!("C"),
-            2 => println!("B"),
-            3 => println!("A"),
-            4 => println!("E"),
-            _ => println!("?"),
-        }
+        let output = results.get(count).unwrap_or(&"?");
+        println!("{}", output);
     }
 }
