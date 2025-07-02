4fc4d6963d3770b9ff6fd670eb748a54b9171218
U-Keun Song <sukeun319@gmail.com>
2025-07-02 10:03:14 +0900

    [Bronze II] Title: 창고지기, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Bronze/31844. 창고지기/창고지기.rs
+++ 백준/Bronze/31844. 창고지기/창고지기.rs
@@ -4,26 +4,13 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
-    let mut r = 0i8; let mut b = 0i8; let mut d = 0i8;
-    for (i, c) in input.chars().enumerate() {
-        match c {
-            '@' => r = i as i8,
-            '#' => b = i as i8,
-            '!' => d = i as i8,
-            _ => {}
-        }
-    }
-
-    let mut answer = -1 as i8;
-    if r < d {
-        if r < b && b < d {
-            answer = d - r - 1;
-        }
-    } else {
-        if d < b && b < r {
-            answer = r - d - 1;
-        }
-    }
+    let answer = match (input.find('@'), input.find('#'), input.find('!')) {
+        (Some(r), Some(b), Some(d))
+            if (b > r && b < d) || (b > d && b < r) => {
+                (r.abs_diff(d) as isize) - 1
+            }
+        _ => -1,
+    };
 
     println!("{}", answer);
 }
