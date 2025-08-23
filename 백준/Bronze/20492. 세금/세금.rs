b4e604f3c019e3c5179e9093add57f81bde87eeb
U-Keun Song <sukeun319@gmail.com>
2025-08-24 01:04:32 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.rs
@@ -0,0 +1,17 @@
+use std::io::{ self, Read };
+
+macro_rules! next {
+    ($it: expr, $t: ty) => {
+        $it.next().unwrap().parse::<$t>().unwrap()
+    };
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut it = buf.split_whitespace();
+
+    let n = next!(it, u64);
+
+    println!("{} {}", n * 78 / 100, n * 956 / 1000);
+}
