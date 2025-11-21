6bdf3c72063b6f1141dd0feb728b089aeea8a6b0
U-Keun Song <sukeun319@gmail.com>
2025-11-21 15:32:15 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 1.94 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long i = 1;
+    while (i * i < n) { i++; }
+    if (i *  i == n) return (i + 1) * (i + 1);
+    return -1;
+}
