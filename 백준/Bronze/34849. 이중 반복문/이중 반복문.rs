5403ff03fc313163d1a502bc293faaf576d5e4e3
U-Keun Song <sukeun319@gmail.com>
2025-12-31 10:02:15 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 13196 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.rs
@@ -0,0 +1,18 @@
+use std::io::{ self, Read };
+
+macro_rules! next {
+    ($it: expr, $t: ty) => {
+        $it.next().unwrap().parse::<$t>().unwrap()
+    };
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut it = buf.split_whitespace();
+
+    let n = next!(it, usize);
+    if n <= 10000 {
+        println!("Accepted");
+    } else { println!("Time limit exceeded") };
+}
